4c1f044495cc69703041e46729e06ec4c0cb4d83
arodriguezgreaves <arodriguezgreaves@trentu.ca>
2025-04-04 15:56:40 -0400

    Revert "task 1) 2"
    This reverts commit 1dc822c55e151040778255b22fb605ae55f03169.

--- Library/RentalSystem.java
+++ Library/RentalSystem.java
@@ -1,9 +1,6 @@
 import java.util.List;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
 
 public class RentalSystem {
     private List<Vehicle> vehicles = new ArrayList<>();
@@ -12,12 +9,10 @@ public class RentalSystem {
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
-        saveVehicle(vehicle);
     }
 
     public void addCustomer(Customer customer) {
         customers.add(customer);
-        saveCustomer(customer);
     }
 
     public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
@@ -25,7 +20,6 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, amount, "RENT"));
             System.out.println("Vehicle rented to " + customer.getCustomerName());
-            saveRecord(record);
         }
         else {
             System.out.println("Vehicle is not available for renting.");
@@ -37,7 +31,6 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, extraFees, "RETURN"));
             System.out.println("Vehicle returned by " + customer.getCustomerName());
-            saveRecord(record);
         }
         else {
             System.out.println("Vehicle is not rented.");
@@ -96,72 +89,4 @@ public class RentalSystem {
                 return c;
         return null;
     }
-    
-    
-    public void saveVehicle(Vehicle vehicle) {
-    	try {
-    	      File myObj = new File("vehicle.txt");
-    	      
-    	    } 
-    	catch (IOException e) {
-    	      System.out.println("A file creation error occurred.");
-    	      e.printStackTrace();
-    	    }
-    	
-    	try {
-    	      FileWriter myWriter = new FileWriter("vehicle.txt");
-    	      myWriter.write(vehicle.toString());
-    	      myWriter.close();
-    	}
-    	
-    	catch (IOException e) {
-    	      System.out.println("A file write error occurred.");
-    	      e.printStackTrace();
-    	}
-    }
-    
-    public void saveCustomer(Customer customer) {
-    	try {
-  	      File myObj = new File("customers.txt");
-  	      
-  	    } 
-  	catch (IOException e) {
-  	      System.out.println("A file creation error occurred.");
-  	      e.printStackTrace();
-  	    }
-  	
-  	try {
-  	      FileWriter myWriter = new FileWriter("customers.txt");
-  	      myWriter.write(customer.toString());
-  	      myWriter.close();
-  	}
-  	
-  	catch (IOException e) {
-  	      System.out.println("A file write error occurred.");
-  	      e.printStackTrace();
-  	}
-    }
-    
-    public void saveRecord(RentalRecord record) {
-    	try {
-    	      File myObj = new File("rental_record.txt");
-    	      
-    	    } 
-    	catch (IOException e) {
-    	      System.out.println("A file creation error occurred.");
-    	      e.printStackTrace();
-    	    }
-    	
-    	try {
-  	      FileWriter myWriter = new FileWriter("rental_record.txt");
-  	      myWriter.write(record.toString());
-  	      myWriter.close();
-  	}
-  	
-  	catch (IOException e) {
-  	      System.out.println("A file write error occurred.");
-  	      e.printStackTrace();
-  	}
-    }
-    
 }
